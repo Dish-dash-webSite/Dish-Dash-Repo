5cd5f606f6b1ac46403f85752fab9d688de39fb5
fahmi-najjari <fahminajjari@outlook.com>
2025-02-04 20:47:58 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,14 @@ const host = process.env.SERVER_HOST
 const db = require("./database/connection.js");
 const app = express();
 
+const userRoutes = require('./routes/userRoutes');
+
 app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
+
+// Use user routes
+app.use('/api/users', userRoutes);
 
 app.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);

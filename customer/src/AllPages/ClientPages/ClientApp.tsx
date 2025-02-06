ac6dfaf09601c14946da25a6e45a60965291aaa6
MehdiIdoudi <mehdiaverus@gmail.com>
2025-02-06 17:14:50 +0100

    checkpoint

--- customer/src/AllPages/ClientPages/ClientApp.tsx
+++ customer/src/AllPages/ClientPages/ClientApp.tsx
@@ -2,12 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage"
 import SignUp from "./pages/Signup"
 import Login from "./pages/Login"
-import 
+
 
 const ClientApp: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="*" element={<Homepage />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
     </Routes>
--- customer/src/AllPages/OwnerRestaurantPages/restoApp.tsx
+++ customer/src/AllPages/OwnerRestaurantPages/restoApp.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Dashboard from "./pages/dashboard";
-// import RestaurantForm from "./pages/CreateRestaurant";
+import RestaurantForm from "./pages/CreateRestaurant";
 import { Routes, Route } from "react-router-dom";
 
 const RestaurantOwner: React.FC = () => {
     return (
         <Routes>
             <Route path="dashboard" element={<Dashboard />} />
-            {/* <Route path="restaurant" element={<RestaurantForm />} /> */}
+            <Route path="restaurant" element={<RestaurantForm />} />
         </Routes>
     );
 };

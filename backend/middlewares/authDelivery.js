d8d787abcbd7c87d47111c48f4d52e572ea12332
MehdiIdoudi <mehdiaverus@gmail.com>
2025-02-09 13:08:16 +0100

    fixed resto auth

--- backend/middlewares/authDelivery.js
+++ backend/middlewares/authDelivery.js
@@ -3,7 +3,8 @@ const JWT_SECRET = process.env.JWT_SECRET || '1234';
 
 const authMiddleware = (req, res, next) => {
     try {
-        const token = req.cookies?.auth_token;
+        const token = req.cookies.token;
+        console.log(token);
         if (!token) return res.status(401).json({ message: 'Unauthorized, no token provided' });
 
         jwt.verify(token, JWT_SECRET, (err, decoded) => {
--- backend/routes/restaurantOwner.js
+++ backend/routes/restaurantOwner.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routerRestOwner = express.Router();
 const Restaurent = require("../controllers/restaurantOwner")
+const authMiddleware = require('../middlewares/authDelivery')
 
 routerRestOwner.get("/all/:id", Restaurent.getAllItem)
 routerRestOwner.put("/:id", Restaurent.updateProfile)
@@ -11,5 +12,5 @@ routerRestOwner.post("/newcat", Restaurent.addCategory)
 routerRestOwner.get("/allItem", Restaurent.getallRestoOfOne)
 routerRestOwner.post("/createResto", Restaurent.CreateRestaurant)
 routerRestOwner.post("/loginResto", Restaurent.LoginRestoOwner)
-routerRestOwner.get("/getResto", Restaurent.getRestoOwner)
+routerRestOwner.get("/getResto",authMiddleware ,Restaurent.getRestoOwner)
 module.exports = routerRestOwner
